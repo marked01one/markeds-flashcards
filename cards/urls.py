2650f31589b7e07e30c48e01fa44290d4de83ded
marked01one <110973167+marked01one@users.noreply.github.com>
2022-10-18 00:09:50 -0700

    Move pages styles into static CSS
    Moved the pages styles into the 'static' folder to prepare to implement responsiveness and a light/dark theme switcher.

--- cards/urls.py
+++ cards/urls.py
@@ -17,5 +17,10 @@ urlpatterns = [
         "edit/<int:pk>",
         views.CardUpdateView.as_view(),
         name="card-update"
+    ),
+    path(
+        "box/<int:box_num>",
+        views.BoxView.as_view(),
+        name="box",
     )
 ]
--- cards/views.py
+++ cards/views.py
@@ -19,5 +19,19 @@ class CardCreateView(CreateView):
     fields: list = ["question", "answer", "box"]
     success_url = reverse_lazy("card-create")
 
+# Class for updating the values of a card
 class CardUpdateView(CardCreateView, UpdateView):
     success_url = reverse_lazy("card-list")
+
+# Class for viewing contents of a single box
+class BoxView(CardListView):
+    template_name: str = "cards/box.html"
+    
+    # Return a queryset of all card objects that fall under a specific box number
+    def get_queryset(self):
+        return Card.objects.filter(box=self.kwargs["box_num"])
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["box_number"] = self.kwargs["box_num"]
+        return context
